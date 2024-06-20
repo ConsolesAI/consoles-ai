dd729b579b6b901d7ccf1611d6a26e8ed057b107
ovived <rob@mtbmedia.com>
2024-06-20 18:00:26 -0400

    cli tools

--- cli/util/file.js
+++ cli/util/file.js
@@ -2,16 +2,24 @@ import * as fs from "fs";
 import chalk from "chalk";
 import figlet from "figlet";
 import crypto from "crypto";
+import { log } from "../util/log.js";
 import readline from "readline";
 
-export const displayFiglet = (text, color = "#4B0082") => {
-  console.log(
-    chalk.hex(color)(figlet.textSync(text, { horizontalLayout: "full" }))
-  );
+export const displayFiglet = async (text, color = "#4B0082") => {
+  return new Promise((resolve) => {
+    figlet(text, { horizontalLayout: "full" }, (err, data) => {
+      if (err) {
+        console.error('Something went wrong...', err);
+        return;
+      }
+      console.log(chalk.hex(color)(data));
+      resolve();
+    });
+  });
 };
 
-export const readEnvFile = (envPath) => {
-  const envContent = fs.readFileSync(envPath, "utf-8");
+export const readEnvFile = async (envPath) => {
+  const envContent = await fs.promises.readFile(envPath, "utf-8");
   return Object.fromEntries(
     envContent
       .split("\n")
@@ -20,9 +28,16 @@ export const readEnvFile = (envPath) => {
   );
 };
 
-export const generateToken = () => crypto.randomBytes(16).toString("hex");
+export const generateToken = async () => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(16, (err, buffer) => {
+      if (err) reject(err);
+      resolve(buffer.toString("hex"));
+    });
+  });
+};
 
-export const waitForKeyPress = () => {
+export const waitForKeyPress = async () => {
   return new Promise((resolve) => {
     const rl = readline.createInterface({
       input: process.stdin,
@@ -40,7 +55,8 @@ export const waitForKeyPress = () => {
   });
 };
 
-export const saveEnvFile = (envPath, token, apiKey) => {
-  fs.writeFileSync(envPath, `TOKEN=${token}\nAPI_KEY=${apiKey}\n`);
+export const saveEnvFile = async (envPath, envData) => {
+  const data = `TOKEN=${envData.TOKEN}\nAPI_KEY=${envData.API_KEY}\n`;
+  await fs.promises.writeFile(envPath, data);
   log.success("API key and token saved to 📁 " + chalk.cyan(envPath));
 };
