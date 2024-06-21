1eddfdd29b84194b25d1083ae70e91f3f5c523c6
ovived <rob@mtbmedia.com>
2024-06-20 22:49:34 -0400

    (cli) added version, help. scaffolding for db, secrets

--- cli/index.js
+++ cli/index.js
@@ -5,6 +5,16 @@ import { setupCommand } from "./cmd/setup.js";
 import { deployCommand } from "./cmd/deploy.js";
 import { kvCommand } from "./cmd/kv.js";
 import { initCommand } from "./cmd/init.js";
+import { secretsCommand } from "./cmd/env.js";
+import { readFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const packageJson = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf8'));
 
 const program = new Command();
 
@@ -33,5 +43,23 @@ program
   .action((action, namespace, key, value) => {
     kvCommand(action, namespace, key, value);
   });
+
+program
+  .command('env [action] [namespace] [key] [value]')
+  .description('Secret Spaces management commands')
+  .action((action, namespace, key, value) => {
+    secretsCommand(action, namespace, key, value);
+  });
+
+program
+  .command('help')
+  .description('Display help information for ConsolesAI commands')
+  .action(() => {
+    program.outputHelp();
+  });
+
+  program
+  .version(packageJson.version, '-v, --version', 'Display the current version of ConsolesAI');
+
 
 program.parse(process.argv);
